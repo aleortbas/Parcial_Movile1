75a5999b60724bc9383a3286c3f19f8eed74cf42
Alejandro Ortiz <aleortbas@gmail.com>
2021-09-15 11:06:37 -0500

    Spinner y captura de datos

--- app/src/main/java/com/example/caempresas/Listado.java
+++ app/src/main/java/com/example/caempresas/Listado.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.sql.Connection;
@@ -12,47 +15,58 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Listado extends AppCompatActivity {
 
-    TextView lista;
-    String Nit,TipoEmpresa,nombre,telefonoEmpresa,direccionEmpresa,correo;
-    String resultado ="";
+    EditText prueba;
+    Spinner lista;
+    String resultado="",Nombre,dato;
+    int IDempresa;
+    ArrayList<String> opciones2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado);
 
+        prueba = findViewById(R.id.datos);
         lista = findViewById(R.id.lista);
+        opciones2 = new ArrayList<String>();
+        opciones2.add("Seleccione una empresa");
 
         new Thread(new Runnable() {
             @Override
             public void run() {
+
                 try {
                     Class.forName("com.mysql.jdbc.Driver");
                     Connection con = DriverManager.getConnection("jdbc:mysql://192.168.0.18:3306/infoempresa", "alejandro", "ZZ11yy33Aa00");
-                    String sql = "SELECT * FROM empresa";
-                    Statement stmt = con.createStatement();
-                    ResultSet result = stmt.executeQuery(sql);
-                    while(result.next()){
-                        Nit = result.getString("nit");
-                        TipoEmpresa = result.getString("tipoEmpresa");
-                        nombre = result.getString("nombre");
-                        telefonoEmpresa = result.getString("telefono");
-                        direccionEmpresa = result.getString("direccion");
-                        correo = result.getString("email");
+                    String sql2 = "SELECT * FROM `empresa`";
+                    Statement stmt2 = con.createStatement();
+                    ResultSet result2 = stmt2.executeQuery(sql2);
+                    while(result2.next()){
 
-                        resultado += String.valueOf(Nit) +" "+ "Nombre: " + nombre + "Tipo: " +TipoEmpresa +" Telefono: " + telefonoEmpresa +
-                                "Direccion :"+direccionEmpresa+"\n" + "\n";
+                        IDempresa = result2.getInt("Id_empresa");
+                        Nombre = result2.getString("nombre");
+                        resultado = String.valueOf("")+ "ID empresa: " + IDempresa + " Nombre: " + Nombre + "\n";
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                lista.setText(resultado);
+                                opciones2.add(resultado);
                             }
                         });
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
                     }
+                    con.close();
+
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 } catch (SQLException throwables) {
@@ -63,10 +77,22 @@ public class Listado extends AppCompatActivity {
 
 
         }).start();
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, opciones2);
+        lista.setAdapter(adapter2);
+
+
     }
 
     public void atras(View view) {
         Intent index =new Intent(this,MainActivity.class);
         startActivity(index);
+    }
+
+    public void Buscar(View view) {
+        dato = lista.getSelectedItem().toString();
+        prueba.setText(dato);
+        Intent empresas =new Intent(this,ListaEmpresas.class);
+        empresas.putExtra("dato", dato);
     }
 }
